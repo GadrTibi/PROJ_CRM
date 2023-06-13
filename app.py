91f7f68b0473819af48cedfd6bb05bf42500c5ab
Gad Tibi <gadtibi@MacBook-Pro-de-Gad.local>
2023-06-13 16:46:09 +0200

    implementation bdd V2.2

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crm.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-@app.before_first_request
-def create_tables():
+with app.app_context():
     db.create_all()
 
 @app.route('/')
--- models.py
+++ models.py
@@ -2,15 +2,18 @@ from database import db
 
 class Lead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    # Ajoutez d'autres champs si nécessaire
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    teleprospector_id = db.Column(db.Integer, db.ForeignKey('teleprospector.id'), nullable=False)
 
 class Teleprospector(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    # Ajoutez d'autres champs si nécessaire
+    name = db.Column(db.String(50), nullable=False)
+    leads = db.relationship('Lead', backref='teleprospector', lazy=True)
 
 class Manager(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    # Ajoutez d'autres champs si nécessaire
+    name = db.Column(db.String(50), nullable=False)
+    teleprospectors = db.relationship('Teleprospector', backref='manager', lazy=True)
