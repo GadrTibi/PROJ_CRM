1a8e33fa39bac1ba801123afb1eee0f89dbfd103
Gad Tibi <gadtibi@MacBook-Pro-de-Gad.local>
2023-06-13 17:04:36 +0200

    implementation bdd V2.4

--- models.py
+++ models.py
@@ -1,17 +1,25 @@
 from database import db
-
-class Lead(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
-    status = db.Column(db.String(20), nullable=False)
-    teleprospector_id = db.Column(db.Integer, db.ForeignKey('teleprospector.id'), nullable=False)
+from sqlalchemy.orm import relationship
 
 class Teleprospector(db.Model):
+    __tablename__ = 'teleprospectors'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    leads = db.relationship('Lead', backref='teleprospector', lazy=True)
+    name = db.Column(db.String(100), nullable=False)
+    leads = relationship('Lead', backref='teleprospector', lazy=True)
 
 class Manager(Teleprospector):
-    teleprospectors = db.relationship('Teleprospector', backref='manager', lazy=True)
+    __tablename__ = 'managers'
+
+    id = db.Column(db.Integer, db.ForeignKey('teleprospectors.id'), primary_key=True)
+    teleprospectors = relationship('Teleprospector', backref='manager', lazy=True)
+
+class Lead(db.Model):
+    __tablename__ = 'leads'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(100), nullable=False)
+    teleprospector_id = db.Column(db.Integer, db.ForeignKey('teleprospectors.id'), nullable=False)
