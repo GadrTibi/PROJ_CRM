31e4750e9dab745856ac495b855bd5a46d6d4d0d
Gad Tibi <gadtibi@MacBook-Pro-de-Gad.local>
2023-06-14 10:55:09 +0200

    implementation bdd V2.7

--- models.py
+++ models.py
@@ -6,8 +6,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     type = db.Column(db.String(50))
